f4bc86e77487be92c83f34bf60b97f23464007ba
Mian Zhong <mian.zhong@berkeley.edu>
2021-08-17 16:25:01 +0200

    revise aes

--- app/app.py
+++ app/app.py
@@ -17,47 +17,6 @@ app.title = "bdpp"
 server = app.server
 
 
-# Plotly mapbox public token
-mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
-
-# Dictionary of important locations in New York
-list_of_locations = {
-    "Madison Square Garden": {"lat": 40.7505, "lon": -73.9934},
-    "Yankee Stadium": {"lat": 40.8296, "lon": -73.9262},
-    "Empire State Building": {"lat": 40.7484, "lon": -73.9857},
-    "New York Stock Exchange": {"lat": 40.7069, "lon": -74.0113},
-    "JFK Airport": {"lat": 40.644987, "lon": -73.785607},
-    "Grand Central Station": {"lat": 40.7527, "lon": -73.9772},
-    "Times Square": {"lat": 40.7589, "lon": -73.9851},
-    "Columbia University": {"lat": 40.8075, "lon": -73.9626},
-    "United Nations HQ": {"lat": 40.7489, "lon": -73.9680},
-}
-
-# Initialize data frame
-df1 = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/uber-rides-data1.csv",
-    dtype=object,
-)
-df2 = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/uber-rides-data2.csv",
-    dtype=object,
-)
-df3 = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/uber-rides-data3.csv",
-    dtype=object,
-)
-df = pd.concat([df1, df2, df3], axis=0)
-df["Date/Time"] = pd.to_datetime(df["Date/Time"], format="%Y-%m-%d %H:%M")
-df.index = df["Date/Time"]
-df.drop("Date/Time", 1, inplace=True)
-totalList = []
-for month in df.groupby(df.index.month):
-    dailyList = []
-    for day in month[1].groupby(month[1].index.day):
-        dailyList.append(day[1])
-    totalList.append(dailyList)
-totalList = np.array(totalList, dtype=object)
-
 # Layout of Dash App
 app.layout = html.Div(
     children=[
@@ -68,14 +27,12 @@ app.layout = html.Div(
                 html.Div(
                     className="four columns div-user-controls",
                     children=[
-                        html.A(
-                            html.Img( # TODO: ETH logo?
-                                className="logo",
-                                src="https://ethz.ch/services/en/service/communication/corporate-design/logo/_jcr_content/par/twocolumn/par_right/fullwidthimage/image.imageformat.twocolumn.122310607.png",
-                            )
+                        html.Img( # TODO: ETH logo?
+                            className="logo",
+                            src="https://ethz.ch/services/en/service/communication/corporate-design/logo/_jcr_content/par/twocolumn/par_right/fullwidthimage/image.imageformat.twocolumn.122310607.png",
                         ),
 
-                        html.H2("Topic Modeling on Nationally Determined Contributions"),
+                        html.H1("Topic Modeling on Nationally Determined Contributions"),
 
                         #Change to side-by-side for mobile layout
                         html.Div(
@@ -126,12 +83,14 @@ app.layout = html.Div(
                         
                         dcc.Markdown(
                             """
-                            Sources: [NDCs](https://www4.unfccc.int/sites/NDCStaging/Pages/Home.aspx)
+                            Sources: [Original UNFCCC NDCs](https://www4.unfccc.int/sites/NDCStaging/Pages/Home.aspx)
+                            
                             Design Credits to: [Dash-sample-apps](https://github.com/plotly/dash-sample-apps/tree/main/apps/dash-uber-rides-demo) 
                             
-                            Disclaimer: Due to time constraint, we use the default map which does not represent authors' belief. 
-                            More to read in [Using Built-in Country and State Geometries](https://plotly.com/python/choropleth-maps/). 
-                            """
+                            Disclaimer: The map features are by definition subject to change, debate and dispute. We use geojsons from [this repository](https://github.com/johan/world.geo.json)
+                            with some modifications. 
+                            """,
+                            style = {"margin-top": "30px"}
                         ),
                     ],
                 ),
@@ -146,9 +105,10 @@ app.layout = html.Div(
                         html.Div(
                             children = [
                                 html.H3(
-                                    """LDA Topic Modeling Visualization:"""
+                                    """LDA Topic Modeling Visualization:""",
+                                    style = {"color":"#1E1E1E"}
                                 ),
-                                html.Iframe(src=app.get_asset_url("lda.html"), style=dict(width="95%", height="400%"))
+                                html.Iframe(src=app.get_asset_url("lda.html"))
                             ]
                         )
                     ],
@@ -203,7 +163,12 @@ def display_map(topic, map_view, continent):
         scope = "world"
 
     if topic is None:
-        choropleth_map = px.choropleth(df, geojson=countries, locations = "ISO", featureidkey = "id", scope=scope)
+        choropleth_map = px.choropleth(df, 
+            locations = "ISO", 
+            geojson=countries, 
+            featureidkey = "id", 
+            color_discrete_sequence = ["lightgrey"],
+            scope=scope)
     else:
         choropleth_map = px.choropleth(
             df, 
@@ -211,7 +176,7 @@ def display_map(topic, map_view, continent):
             color =  df[topic],
             locations = "ISO",
             featureidkey = "id",
-            range_color = [0, 1],
+            range_color = [0, max(df[topic])],
             color_continuous_scale="bugn",
             scope=scope
         )
@@ -219,10 +184,13 @@ def display_map(topic, map_view, continent):
         title_text = "NDCs Topic Intensity", #TODO
         geo = dict(
             showframe=False,
-            showcoastlines=True,
-            projection_type = 'equirectangular'
+            showcoastlines=False,
+            projection_type = 'equirectangular',
+            visible=False
         ),
-        margin={"r":0, "l":0, "b":0}
+        margin={"r":0, "l":0, "b":0},
+        geo_bgcolor = "#dbdbdb",
+        paper_bgcolor = "#dbdbdb"
     )
     return choropleth_map
 if __name__ == "__main__":
